d66bfa1adb0144539a832db71e01aab392688ae5
FortiethAtom4 <txbbrand@gmail.com>
2022-10-25 13:55:06 -0400

    sadfr

---
+++ songlist.js
@@ -0,0 +1,74 @@
+let data = [
+    {
+        "name": "Last Train at 25 O'Clock",
+        "artist": "Lamp",
+        "year": "2004",
+        "genre": "Indie/Alternative"
+    },
+    {
+        "name": "Notice",
+        "artist": "Moe Shop",
+        "year": "2018",
+        "genre": "EDM"
+    },
+    {
+        "name": "Spong Bob",
+        "artist": "Hamada Helal",
+        "year": "2012",
+        "genre": "Pop"
+    },
+    {
+        "name": "Career Day",
+        "artist": "The Format",
+        "year": "2003",
+        "genre": "Indie/Alternative"
+    },
+    {
+        "name": "Surfin' USA",
+        "artist": "The Beach Boys",
+        "year": "1963",
+        "genre": "Rock & Roll"
+    },
+    {
+        "name": "Etudes, Op. 25 No. 11 in A Minor",
+        "artist": "Frederic Chopin",
+        "year": "1836",
+        "genre": "Classical"
+    },
+    {
+        "name": "Call Me Maybe",
+        "artist": "Carly Rae Jepsen",
+        "year": "2012",
+        "genre": "Pop"
+    },
+    {
+        "name": "All I want for Christmas is You",
+        "artist": "Mariah Carey",
+        "year": "1994",
+        "genre": "Pop"
+    },
+    {
+        "name": "Tokyo Drift",
+        "artist": "Teriyaki Boyz",
+        "year": "2006",
+        "genre": "Rap"
+    },
+    {
+        "name": "Thrift Shop",
+        "artist": "Macklemore & Ryan Lewis",
+        "year": "2012",
+        "genre": "Rap"
+    },
+    {
+        "name": "Witch Doctor",
+        "artist": "Cartoons",
+        "year": "1998",
+        "genre": "Children's Music"
+    },
+    {
+        "name": "Lagtrain",
+        "artist": "Inabakumori",
+        "year": "2020",
+        "genre": "J-Pop"
+    }
+]
